0c6a3cf756ff92c2952ba04c33586283c9f683ba
AnnaClaire Marley <annaclairemarley@gmail.com>
2020-08-15 14:10:26 -0700

    adjusted correlation graph function to be better for paper

--- Functions/Graphing/graph_correlation.R
+++ Functions/Graphing/graph_correlation.R
@@ -14,18 +14,28 @@ graph_correlation = function(df, x_data, y_data, xlabel = "", ylabel = "", title
   rmse_s = rmse(x_data, y_data)
   mae_s = mae(x_data, y_data)
   
+  df <- df %>% 
+    mutate(month = factor(month, levels=c("Nov","Dec", "Jan", "Feb", "Mar", "Apr")))
+  
     # graph time series and add linear trendline 
     plot <- ggplot(df, aes(x = x_data, y = y_data)) +
-      geom_point() +
+      geom_point(aes(color = month ), size = 2) +
+      geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
       labs(
         title = sprintf("%s", title),
         x = sprintf("%s", xlabel),
         y = sprintf("%s", ylabel),
         subtitle = sprintf("RMSE: %s mm | MAE: %s mm", round(rmse_s,2), round(mae_s,2))
       ) +
-    #  scale_x_continuous(expand = c(0,0)) +
-     # scale_y_continuous(expand = c(0,0)) +
-      theme_bw(base_size = 14)
+      scale_color_manual(values = colors) +
+      scale_x_continuous(expand = c(0,0)) +
+      scale_y_continuous(expand = c(0,0)) +
+      coord_cartesian(clip = 'off') +
+      theme_classic() +
+      theme(text = element_text(size=14),
+            axis.line.y.right = element_line(color = "black"),
+            axis.line.x.top = element_line(color = "black"),
+            panel.border = element_rect(colour = "black", fill=NA))  
     
   return(plot)
   
